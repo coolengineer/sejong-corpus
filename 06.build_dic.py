3e8609cc49f350070ecd5137f4336c0aa5971a34
Hojin Choi <hojin.choi@gmail.com>
2018-12-13 15:20:42 +0900

    Change internal format

--- 06.build_dic.py
+++ 06.build_dic.py
@@ -8,65 +8,89 @@ from io import open
 from bs4 import BeautifulSoup
 import sys
 import os
+import re
 
-try: os.mkdir( 'logs' )
-except: pass
+def analyze_chunk(outf, text):
+	count = 0
+	try:
+		for f in text:
+			f = f.replace("::","")
+			for line in f.split('\n'):
+				tabidx = line.find('\t')
+				if tabidx < 0:
+					raise Exception("Invalid line: [%s]" % line)
+				a = line[tabidx+1:]
+				for b in re.split(' ?\+ ?', a):
+					b = b.strip()
+					slashidx = b.rfind('/')
+					if slashidx < 0:
+						continue
+					outf.write( '%s\n' % b )
+				count += 1
+	except Exception as e:
+		#print(e)
+		pass
+	return count
 
-outf = open('logs/word-analysis.txt', 'w')
-outp = open('logs/pos-analysis.txt', 'w')
-count = 0
-
-def analyze(text):
-	global count
+def analyze_type1(outf, text):
+	count = 0
 	content = ''
-	for s in str(text).split('\n'):
+	for t in text.split('\n'):
 		try:
-			(dummy,line) = s.split('\t',1)
+			#Screening only tab contained lines
+			(dummy,line) = t.split('\t',1)
 			content = content + line + '\n'
-		except:
-			pass
-	doc = BeautifulSoup( content, 'html.parser' )
-	for e in doc.find_all('s'):
-		try:
-			for f in e.stripped_strings:
-				posarr = []
-				f = f.encode('utf8').replace("::","")
-				for line in f.split('\n'):
-					if line.find('\t') < 0:
-						raise Exception("Invalid line: %s" % line)
-					a = line[line.find('\t')+1:]
-					word_posarr = []
-					for b in a.split('+'):
-						if b.find('/') < 0:
-							raise Exception("Invalid line: %s" % line)
-						word_posarr.append( b[b.find('/')+1:] )
-					while len(word_posarr) > 0:
-						if word_posarr[-1][0] != 'S':
-							break
-						word_posarr = word_posarr[0:-1]
-					posarr.append( "+".join(word_posarr) )
-				if len(posarr) > 0:
-					p = " ".join(posarr)
-					outf.write( '%s\n' % f )
-					outp.write( '%s\n' % p )
-					count = count + 1
 		except Exception as e:
 			pass
-		#print "-----------".join(words)
+	count += analyze_chunk(outf, [content])
+	return count
 
-def extract(path):
-	global count
-	doc = BeautifulSoup(open(path,'rb'), 'html.parser')
-	#doc = ElementTree.parse(source,parser).getroot()
-	if len(doc.find_all('body')) != 0:
-		return
-	print "Process:", path, 
+def analyze_type2(outf, text):
 	count = 0
-	for text in doc.find_all('text'):
-		analyze(text)
-	print ":", count
+	content = []
+	for t in text.split('\n'):
+		try:
+			#Screening only tab contained lines
+			t = re.sub('<phon>.*</phon>', '', t)
+			(dummy,line) = t.split('\t',1)
+			content.append(line)
+		except Exception as e:
+			pass
+
+	count += analyze_chunk(outf, content)
+	return count
+
+def extract(outf, path, idx, total):
+	count = 0
+	content = open(path, encoding='utf-8').read()
+	doc = BeautifulSoup(content, 'html.parser')
+
+	texts = doc.select('text body p')
+	for text in texts:
+		text = text.get_text()
+		count += analyze_type1(outf, text)
+
+	texts = doc.select('text s')
+	for text in texts:
+		text = text.get_text()
+		count += analyze_type2(outf, text)
+	print("(%d/%d) Extract %s: %d morphemes" % (idx, total, path, count) )
 
 if __name__ == '__main__':
-    for p in sys.argv[1:]:
-        extract(p)
+	try:
+		outfile = sys.argv[1]
+		outf = open(outfile, mode='wt', encoding='utf-8')
+		files = sys.argv[2:]
+		total = len(files)
+		count = 0
+		for p in files:
+			count += 1
+			extract(outf, p, count, total)
+	except KeyboardInterrupt:
+		print("\nOk, take a rest!")
+	except:
+		raise
+
+outf.close()
+
 # vim: ts=4 noexpandtab sw=4 sts=4
