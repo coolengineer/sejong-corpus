ed2a9ece4fabed7d4621e98c12dcaa0ae3e131be
Hojin Choi <hojin.choi@gmail.com>
2018-12-17 00:34:39 +0900

    Refine skipping condition

--- 05.convert.sh
+++ 05.convert.sh
@@ -5,27 +5,25 @@
 # vim: ts=4 noexpandtab sw=4 sts=4
 
 cd $(dirname $0)
-mkdir -p corpus-utf8
+DESTDIR=${DESTDIR-corpus-utf8}
+mkdir -p "$DESTDIR"
 rm -f logs/unknown-xml-tags.log
 find corpus -name '[0-9A-Z]*[A-Z]*[0-9A-Z]*.txt' | 
 	while read INFILE
 	do
-		OUTFILE=${INFILE/corpus/corpus-utf8}
-		LOGFILE=${INFILE%.txt}.log
+		OUTFILE=${INFILE/corpus/$DESTDIR}
 		if test -f $OUTFILE; then
-			echo "Skip $INFILE > $OUTFILE"
-			continue
+			PARTIAL=$(head -2 $OUTFILE)
+			if test -n "$PARTIAL"; then
+				if test "$OUTFILE" -nt "$INFILE"; then
+					echo "Skip $INFILE > $OUTFILE"
+					continue
+				fi
+			fi
 		fi
 
-		./05.convert-file.sh $INFILE $OUTFILE 2>>$LOGFILE
-
-		## Patching
-		PATCHES=${INFILE/corpus/patches}
-		PATCHES=${PATCHES%.txt}-*.patch
-		for patch in $PATCHES
-		do
-			test -f $patch || continue
-			echo "Patching ... $patch"
-		done
+		./05.convert-file.sh $INFILE $OUTFILE
 	done
-sort logs/unknown-xml-tags.log | uniq -c | sort -nr > logs/unknown-xml-tags2.log
+if test -f logs/unknown-xml-tags.log; then
+	sort logs/unknown-xml-tags.log | uniq -c | sort -nr > logs/unknown-xml-tags2.log
+fi
