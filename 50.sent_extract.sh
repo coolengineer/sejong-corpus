450b26e51bbc759830be132722eaf0656db068f8
Hojin Choi <hojin.choi@gmail.com>
2019-05-09 16:44:10 +0900

    Fixed file name and permission

--- 50.sent_extract.sh
+++ 50.sent_extract.sh
@@ -29,7 +29,7 @@ do
     SENTFILE=${INFILE/corpus-utf8/$SENTDIR}
     SENTFILE=${SENTFILE%.txt}.sent.txt
     if test "$INFILE" -nt "$SENTFILE"; then
-        ./51.extract.py $INFILE $SENTFILE 2>> $LOGFILE || { echo "Check $LOGFILE"; exit 1; }
+        ./51.sent_extract.py $INFILE $SENTFILE 2>> $LOGFILE || { echo "Check $LOGFILE"; exit 1; }
     else
         echo "($COUNT/$TOTAL) Skip extract sentence $INFILE >> $SENTFILE"
     fi
@@ -39,5 +39,5 @@ $(find corpus-utf8 \( -name '?CT_*.txt' -o -name '?T*.txt' \))
 EOT
 
 echo "Concatenate tmp/s/*.sent.txt > $OUTFILE"
-find tmp/m -name '*.sent.txt' -exec cat {} \; > $OUTFILE 2>>$LOGFILE
+find tmp/s -name '*.sent.txt' -exec cat {} \; > $OUTFILE 2>>$LOGFILE
 echo "Log file: $LOGFILE"
